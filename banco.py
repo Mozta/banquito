cfc4ac2477ddd1b61d56cf378d825032f6f5f983
mozta <perezaguirrerafael@gmail.com>
2024-02-12 16:42:17 -0600

    add postgres

--- banco.py
+++ banco.py
@@ -1,17 +1,33 @@
 from cuenta import Cuenta
+from database import DB
 
 class Banco:
     def __init__(self):
         self.cuentas = {}
+        self.db = DB()
+        self.cuentas = {}
+        print("Obteniendo cuentas de la DB...")
+        self.obtener_cuentas_db()
 
-    def crear_cuenta(self, id_cuenta):
-        if id_cuenta not in self.cuentas:
-            self.cuentas[id_cuenta] = Cuenta(id_cuenta)
-            return True
+    def crear_cuenta(self):
+        # if id_cuenta not in self.cuentas:
+        #     self.cuentas[id_cuenta] = Cuenta(id_cuenta)
+        #     return True
+        self.db.crear_cuenta()
         return False
 
     def obtener_cuenta(self, id_cuenta):
         return self.cuentas.get(id_cuenta, None)
+    
+    def obtener_cuentas(self):
+        return self.cuentas
+    
+    def obtener_cuentas_db(self):
+        cuentas = self.db.obtener_cuentas()
+        for cuenta in cuentas:
+            id = cuenta[0]
+            saldo = cuenta[1]
+            self.cuentas[id] = Cuenta(id, saldo)
 
     def transferir(self, id_origen, id_destino, monto):
         cuenta_origen = self.obtener_cuenta(id_origen)
@@ -29,3 +45,7 @@ class Banco:
     
     def serializar_cuentas(self):
         return [self.serializar_cuenta(cuenta) for cuenta in self.cuentas.values()]
+    
+
+# db = DB()
+# db.obtener_cuentas()
--- database.py
+++ database.py
@@ -1,15 +1,14 @@
 import psycopg2
 
 # db = 'banquito'
-user = 'admin'
-password = 'admin'
-host = 'localhost'
-port = '5432'
+# user = 'postgres'
+# password = '1234'
+# host = 'localhost'
+# port = '5432'
 
-conn = psycopg2.connect(f"user={user} password={password} host={host} port={port}")
-cur = conn.cursor()
+# conn = psycopg2.connect(f"user={user} password={password} host={host} port={port} dbname={db}")
+# cur = conn.cursor()
 
-cur.execute("create DATABASE banquito")
 # cur.execute("""
 #     CREATE TABLE IF NOT EXISTS cuentas (
 #         id SERIAL PRIMARY KEY,
@@ -17,6 +16,60 @@ cur.execute("create DATABASE banquito")
 #     )
 # """)
 
-cur.commit()
-cur.close()
-conn.close()
+# # cur.execute("INSERT INTO cuentas (saldo) VALUES(%s)", (500.0,) )
+# cur.execute("SELECT * FROM cuentas")
+# rows = cur.fetchall()
+# for row in rows:
+#     print(row)
+
+# conn.commit()
+# cur.close()
+
+
+class DB:
+    def __init__(self):
+        db = 'banquito'
+        user = 'postgres'
+        password = '1234'
+        host = 'localhost'
+        port = '5432'
+        print("Inicializando database...")
+        self.conn = psycopg2.connect(f"user={user} password={password} host={host} port={port} dbname={db}")
+        self.cur = self.conn.cursor()
+
+    def mostrar_cuentas(self):
+        self.cur.execute("SELECT * FROM cuentas")
+        rows = self.cur.fetchall()
+        for row in rows:
+            print(row)
+        self.conn.commit()
+
+    def obtener_cuentas(self):
+        self.cur.execute("SELECT * FROM cuentas")
+        rows = self.cur.fetchall()
+        for row in rows:
+            print(row)
+        self.conn.commit()
+        return rows
+
+    def crear_cuenta(self):
+        try:
+            self.cur.execute("INSERT INTO cuentas (saldo) VALUES(%s)", (0.0,) )
+            self.conn.commit()
+            return True
+        except:
+            return False
+
+    def actualizad_saldo(self, id, saldo):
+        self.cur.execute("UPDATE cuentas SET saldo = %s WHERE id = %s", (saldo, id))
+        self.conn.commit()
+
+    def cerrar_db(self):
+        self.cur.close()
+
+
+db = DB()
+# db.crear_cuenta()
+db.mostrar_cuentas()
+db.actualizad_saldo(2, 100)
+db.mostrar_cuentas()
